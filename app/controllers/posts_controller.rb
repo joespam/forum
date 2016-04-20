de337200efee22da78606616ef19859229e17e93
Joe Spampinato <jcspampinato@gmail.com>
2016-04-20 12:52:00 -0400

    add authentication to app

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
 	before_action :find_post, only: [:show,:edit,:update,:destroy]
+	before_action :authenticate_user!, except: [:index,:show]
 
 	def create
 		@post = current_user.posts.build(post_params)
